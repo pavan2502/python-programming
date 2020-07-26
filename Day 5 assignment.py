4d12cb7ae7e4b01544dfa01ee72ee5bf500bcf41
pavan2502 <67194944+pavan2502@users.noreply.github.com>
2020-07-26 17:06:51 +0530

    Add files via upload

---
+++ Day 5 assignment.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+l1=[10,20,40,60,70,80]
+l2=[5,15,25,35,45,60]
+for x in l2:
+ l1.append(x)
+l1.sort()
+print(l1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
